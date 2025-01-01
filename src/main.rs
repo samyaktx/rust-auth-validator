cbdade3ae97f28c31cf24a3a84c227f6ac01852f
Samyakt <isamyakt17@gmail.com>
2025-01-01 21:07:20 +0530

    refactor: simplified main function in src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,6 @@
-use axum::{routing::get, Router};
-use tokio::net::TcpListener;
-use tracing::info;
-use tracing_subscriber::FmtSubscriber;
-
+use auth_boilerplate::run;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tracing::subscriber::set_global_default(FmtSubscriber::default())?;
-
-    let app = Router::new().route("/", get( || async { "Hello, World!" }));
-
-    let listener = TcpListener::bind("127.0.0.1:3000").await?;
-    info!("listening on {}", listener.local_addr()?);
-
-    axum::serve(listener, app.into_make_service())
-        .await?;
-
-    Ok(())
+async fn main() {
+    run().await.unwrap();
 }
