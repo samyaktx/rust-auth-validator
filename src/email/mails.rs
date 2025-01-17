a80ded3bf07b666eb68735f69107cb4532055dd1
Samyakt <isamyakt17@gmail.com>
2025-01-17 15:43:05 +0530

    feat: add email utility functions for verification, welcome, and password reset
    - Implemented 'send_verification_email' to send email verification links.
      - Includes a helper function 'create_verification_link' to generate the verification URL.
    - Implemented 'send_welcome_email'to send a welcome email to new users.
    - Implemented 'send_password_reset_email' to send password reset links.
    - Each function supports dynamic HTML templates with placeholder replacements.
    - Templates referenced:
      - 'src/email/templates/verfication_email.html'
      - 'src/email/templates/welcome_email.html'
      - 'src/email/templates/resetpassword_email.html'
    
    This update adds reusable functions to streamline email communication for user actions.

---
+++ src/email/mails.rs
@@ -0,0 +1,50 @@
+use super::sendmail::send_email;
+
+pub async fn send_verification_email(
+    to_email: &str, 
+    username: &str,
+    token: &str
+) -> Result<(), Box<dyn std::error::Error>> {
+    let subject = "Email Verification";
+    let template_path = "src/email/templates/verfication_email.html";
+    let base_url = "http://localhost:8000/api/v1/auth/verify";
+    let verification_link = create_verification_link(base_url, token);
+    let placeholders = vec![
+        ("{{username}}".to_string(), username.to_string()),
+        ("{{verification_link}}".to_string(), verification_link)
+    ];
+
+    send_email(to_email, subject, template_path, &placeholders).await
+}
+
+fn create_verification_link(base_url: &str, token: &str) -> String {
+    format!("{}?token={}", base_url, token)
+}
+
+pub async fn send_welcome_email(
+    to_email: &str,
+    username: &str
+) -> Result<(), Box<dyn std::error::Error>> {
+    let subject = "Welcome to Application!";
+    let template_path = "src/email/templates/welcome_email.html";
+    let placeholders = vec![
+        ("{{username}}".to_string(), username.to_string())
+    ];
+
+    send_email(to_email, subject, template_path, &placeholders).await
+}
+
+pub async fn send_password_reset_email(
+    to_email: &str,
+    username: &str,
+    reset_link: &str
+) -> Result<(), Box<dyn std::error::Error>> {
+    let subject = "Reset Your Password";
+    let template_path = "src/email/templates/resetpassword_email.html";
+    let placeholders = vec![
+        ("{{username}}".to_string(), username.to_string()),
+        ("{{reset_link}}".to_string(), reset_link.to_string())
+    ];
+
+    send_email(to_email, subject, template_path, &placeholders).await
+}
--- src/email/mod.rs
+++ src/email/mod.rs
@@ -1,1 +1,2 @@
 pub mod sendmail;
+pub mod mails;
